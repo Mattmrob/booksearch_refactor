61d6f81fce719d7e1b0b6c39b867c759f55cd933
Mysmelf <matthewr9937@gmail.com>
2024-01-18 15:41:44 -0800

    Signup function works properly on frontend, need to implement login, add books, and remove books

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -25,16 +25,15 @@ const SignupForm = () => {
     console.log(userFormData)
 
     // check if form has everything (as per react-bootstrap docs)
-    // const form = event.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   event.preventDefault();
-    //   event.stopPropagation();
-    // }
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
     try {
       const { data } = await addUser({ variables: { ...userFormData } });
 
-      console.log(user);
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
